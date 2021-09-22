2fdafba42f142d1c7bb84e15b2cbb40a97e0f19d
Raphaël Diacamille <49157684+rdia9@users.noreply.github.com>
2021-09-22 15:34:05 +0200

    Create list-domain.py

---
+++ list-domain.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+#
+
+import ovh
+
+# Instantiate. Visit https://api.ovh.com/createToken/?GET=/me
+# # to get your credentials
+client = ovh.Client(
+	endpoint='ovh-eu',
+	application_key='XXXXXXXXXXXXXXXXXXXXXXX',
+	application_secret='XXXXXXXXXXXXXXXXXXXXXXX',
+	consumer_key='XXXXXXXXXXXXXXXXXXXXXXX',
+)
+# Print every domain you have
+print("Welcome", client.get('/domain/zone/'))
