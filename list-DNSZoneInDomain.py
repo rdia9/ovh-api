69748e815d52b70e1131bfafb819c7384695bb09
su-rdi <raphael.diacamille@btp-consultants.fr>
2021-10-18 16:58:33 +0200

    Co-authored-by: Paul Baudrier <paul.baudrier.freelance@gmail.com>

--- list-DNSZoneInDomain.py
+++ list-DNSZoneInDomain.py
@@ -5,15 +5,16 @@ First, install the latest release of Python wrapper: $ pip install ovh
 '''
 import json
 import ovh # export ovh api
+import os # pour récupérer les variables d'env
 import re # import regex
 
 # Instantiate. Visit https://api.ovh.com/createToken/?GET=/me
 # # to get your credentials
 client = ovh.Client(
-	endpoint= "${{ ovh_endpoint }}" ,
-	application_key= "${{ ovh_application_key }}" ,
-	application_secret= "${{ ovh_application_secret }}" ,
-	consumer_key= "${{ ovh_consumer_key }}" ,
+	endpoint= os.environ['ovh_endpoint'] ,
+	application_key= os.environ['ovh_application_key'] ,
+	application_secret= os.environ['ovh_application_secret'] ,
+	consumer_key= os.environ['ovh_consumer_key'] ,
 )
 # Print dns zone for each domain
 
@@ -21,14 +22,29 @@ domains = client.get('/domain/zone/')
 for domain in domains:
     details = client.get('/domain/zone/%s/export' % domain)
     detailssansovh = re.sub('.*.ovh.net.*', '', details)
-    regex1='.*IN.A.*'
-    regex2='.*IN.CNAME.*'
+    regex1 = '.*IN.A.*'
+    regex2 = '.*IN.CNAME.*'
     regexList = [regex1, regex2]
+
+    print('"domain";"subdomain";"type";"record"')
+
     for regex in regexList:
         filtereddetails = re.findall(regex, detailssansovh)
-        print ('--------------') # to remove to have correct json
-        print (domain)           # to remove to have correct json
-        print ('--------------') # to remove to have correct json
-        print(json.dumps(filtereddetails, indent=4))
+        for finding in filtereddetails:
+            tmp = finding.split()
 
+            # cas sous domaine vide
+            if(tmp[0]=='IN'):
+                tmp.insert(0,'')
 
+            # remove 'IN'
+            tmp.remove('IN')
+
+            # add domain
+            tmp.insert(0, domain)
+
+            for i, elem in enumerate(tmp):
+                print('"' + elem + '"', end='')
+                if(i != len(tmp)-1):
+                    print(';', end='')
+            print()
