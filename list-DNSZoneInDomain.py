f202dc92672cab89ecf101de63da533af49b070e
su-rdi <raphael.diacamille@btp-consultants.fr>
2021-11-23 12:29:38 +0100

    fix headers and possible env local

--- list-DNSZoneInDomain.py
+++ list-DNSZoneInDomain.py
@@ -7,16 +7,21 @@ import json
 import ovh # export ovh api
 import os # pour récupérer les variables d'env
 import re # import regex
+from decouple import config
 
 
 # Instantiate. Visit https://api.ovh.com/createToken/?GET=/me
 # # to get your credentials
 client = ovh.Client(
-	endpoint= os.environ['ovh_endpoint'] ,
-	application_key= os.environ['ovh_application_key'] ,
-	application_secret= os.environ['ovh_application_secret'] ,
-	consumer_key= os.environ['ovh_consumer_key'] ,
+	endpoint= config('ovh_endpoint') ,
+	application_key= config('ovh_application_key') ,
+	application_secret= config('ovh_application_secret') ,
+	consumer_key= config('ovh_consumer_key') ,
 )
+
+# print headers
+print('"domain";"subdomain";"type";"record"')
+
 # Print dns zone for each domain
 
 domains = client.get('/domain/zone/')
@@ -27,7 +32,7 @@ for domain in domains:
     regex2 = '.*IN.CNAME.*'
     regexList = [regex1, regex2]
 
-    print('"domain";"subdomain";"type";"record"')
+
 
     for regex in regexList:
         filtereddetails = re.findall(regex, detailssansovh)
